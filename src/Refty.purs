453d4aa384dd82ea2200cefcd6d086b1f6af4b96
Shinya Takahashi <s.takahashi313@gmail.com>
2017-06-02 23:47:51 +0900

    Tmp

--- src/Refty.purs
+++ src/Refty.purs
@@ -1,24 +1,44 @@
 module Refty where
 
+import Prelude
 import Data.Exists (Exists)
+import Data.Foreign (Foreign, toForeign)
+import Data.Foreign.Class (class Encode, encode)
 import Data.Maybe (Maybe)
+import Data.StrMap (StrMap, singleton, fromFoldable)
+import Data.Tuple (Tuple(..))
 
 
 
 type Key = String
 
-type Identifier i a = a -> i
+type Identifier a = a -> String
 
-data Entity i a
-  = Individual Key (Identifier i a) a
-  | Collection Key (Identifier i a) (Array a)
+data Entity a
+  = Individual Key (Identifier a) a
+  | Collection Key (Identifier a) (Array a)
 
-data Reference i a
-  = HasOne Key (Identifier i a)
-  | HasMany Key (Identifier i a)
+data Reference a
+  = HasOne Key (Identifier a)
+  | HasMany Key (Identifier a)
 
-data AssociationF i a = AssociationF (Entity i a) (Maybe (Reference i a))
+data AssociationF a = AssociationF (Entity a) (Maybe (Reference a))
 
-type Association i = Exists (AssociationF i)
+type Association = Exists AssociationF
 
-data Formatter i a = Formatter (Entity i a) (Array (Association i))
+data Encoder a = Encoder (Entity a) (Array Association)
+
+
+mapify :: forall a. Encode a => Entity a -> StrMap (StrMap Foreign)
+mapify (Individual k i a) = singleton k $ singleton (i a) $ encode a
+mapify (Collection k i as) = singleton k $ fromFoldable $ map (\a -> Tuple (i a) (encode a)) as
+
+-- TODO toEntities関数
+
+-- TODO toReferences関数
+
+instance encoderEncode :: Encode a => Encode (Encoder a) where
+  encode encoder = toForeign
+    { entities: [] -- toEntities関数にencoderを渡す
+    , references: [] -- toReferences関数にencoderを渡す
+    }

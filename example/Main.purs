cd75e2ed1b3b39f22de6a7f729c06524580b8c0e
Shinya Takahashi <s.takahashi313@gmail.com>
2017-06-08 17:27:31 +0900

    Added example

--- example/Main.purs
+++ example/Main.purs
@@ -6,7 +6,7 @@ import Data.Foreign.Generic (defaultOptions, genericEncode, encodeJSON)
 import Data.Generic.Rep (class Generic)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Refty (Format, self, concat, collection, response, (~), (^), (|<|))
+import Refty (Format, Params, self, concat, individual, collection, response, (~), (^), (|-|), (|<|))
 
 
 newtype User = User { id :: String, name :: String }
@@ -30,6 +30,11 @@ users =
   , User { id: "2", name: "mdaisuke" }
   ]
 
+
+user :: User
+user = User { id: "3", name: "dongri" }
+
+
 comments :: Array Comment
 comments =
   [ Comment { id : "3", userId: "1", content: "comment 1" }
@@ -37,11 +42,29 @@ comments =
   , Comment { id : "2", userId: "1", content: "comment 2" }
   ]
 
+userParams :: Params User
+userParams = "users" ~ (\(User u) -> u.id) ^ self "self"
+
+commentParams :: Params Comment
+commentParams = "comments" ~ (\(Comment c) -> c.id) ^ "userComments" |<| (\(Comment c) -> c.userId)
+
+commentParams2 :: Params Comment
+commentParams2 = "comments" ~ (\(Comment c) -> c.id) ^ "userComment" |-| (\(Comment c) -> c.userId)
+
 formatter :: Format
 formatter = concat
-  [ collection users $ "users" ~ (\(User u) -> u.id) ^ self "self"
-  , collection comments $ "comments" ~ (\(Comment c) -> c.id) ^ "userComments" |<| (\(Comment c) -> c.userId)
+  [ collection users userParams
+  , collection comments commentParams
+  ]
+
+formatter2 :: Format
+formatter2 = concat
+  [ individual user userParams
+  , collection comments commentParams2
   ]
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = log $ encodeJSON $ response formatter
+main = do
+  log $ "Collection: " <> (encodeJSON $ response formatter)
+  log $ "Individual: " <> (encodeJSON $ response formatter2)
+
